cae71f2bc5a0e92ebe71a32a9a1e96589d5d947c
seel <459745355@qq.com>
2020-12-11 09:57:34 +0800

    update lidar2birdview.py

--- longshaw/lidar2birdview.py
+++ longshaw/lidar2birdview.py
@@ -31,9 +31,10 @@ class Self_Awareness:
 
         poses = genfromtxt(os.path.join(data_dir, 'trajectory.csv'), delimiter=',')
 
-        self.QUEUE_NUM = 5
+        self.QUEUE_NUM = 10
         self.veldynes = []
         self.odoms = []
+        num_frames = 10000
 
         index = 0
         index_total = poses.shape[0]
@@ -50,21 +51,16 @@ class Self_Awareness:
             self.save_vels(vels, file_name=os.path.join(data_dir, 'imgs/{0:06d}.png'.format(i)))
 
 
-    def get_relative_transform(self, q_old, q_t):
+    def transform_point_cloud(self, pc, T):
 
-        M_old = self.convet_quaternion_to_matrix(q_old)
-        M_t = self.convet_quaternion_to_matrix(q_t)
+        pc = np.hstack([pc, np.ones([pc.shape[0], 1])])
 
-        t = np.array([q_old[0:3]-q_t[0:3]])
+        # transform to the local frame
+        pc = np.matmul(T, pc.transpose())
 
-        R_old = M_old[0:3, 0:3]
-        R_t = M_t[0:3, 0:3]
-        deltaT = np.matmul(np.linalg.inv(R_t), R_old)
+        pc_transformed = pc.transpose()[:, :3]
 
-        deltaT = np.hstack([deltaT, t.transpose()])
-        deltaT = np.vstack([deltaT, np.array([[0, 0, 0, 1]])])
-
-        return deltaT
+        return pc_transformed
 
     def update_velodyne_queue(self, vel, pose):
 
@@ -73,50 +69,26 @@ class Self_Awareness:
         if len(self.veldynes) >= self.QUEUE_NUM:
             self.veldynes.pop(0)
 
-        if vel.shape[0] > 0:
-            self.veldynes.append(vel)
+        # transform the current scan from local to global frame (mapping)
+        T = self.convet_quaternion_to_matrix(pose)
+        #vel = self.transform_point_cloud(vel, T)
+        self.veldynes.append(vel)
 
         # concate the velendy scans
         vels = self.veldynes[0]
         for i in range(1, len(self.veldynes)):
             vels = np.vstack([vels, self.veldynes[i]])
 
-        vels = np.hstack([vels, np.ones([vels.shape[0], 1])])
+        print(np.min(vels[:, 2]), np.max(vels[:, 2]))
 
-        # transform to the local frame
-        T = self.convet_quaternion_to_matrix(pose)
-        vels = np.matmul(np.linalg.inv(T), vels.transpose())
+        # transform the submap from global map frame to local lidar frame
+        T2 = np.linalg.inv(T)
+        vels2 = self.transform_point_cloud(vels, T2)
 
-        vels = vels.transpose()[:, :3]
+        print(np.min(vels2[:, 2]), np.max(vels2[:, 2]))
 
-        return vels
+        return vels2
 
-    def concat_scans(self):
-
-        if len(self.veldynes) >= self.QUEUE_NUM:
-            self.veldynes.pop(0)
-            self.odoms.pop(0)
-
-        # start with the current scan
-        scans = np.asarray(self.veldynes[-1])
-        curr_pose = self.odoms[-1]
-
-        assert len(self.veldynes) == len(self.odoms) and len(self.veldynes) > 0
-
-        # process the previous scans
-        for i in range(len(self.veldynes)-1):
-            scan = np.asarray(self.veldynes[i])
-
-            deltaT = self.get_relative_transform(self.odoms[i], curr_pose)
-            deltaT[0:3,0:3] = np.identity(3)
-
-            scan = np.hstack([scan, np.ones([scan.shape[0], 1])])
-            scan2 = np.matmul(deltaT, scan.transpose())
-
-            scan2 = scan2.transpose()[:, :3]
-            scans = np.vstack([scans, scan2])
-
-        return scans
 
     def convet_quaternion_to_matrix(self, pose):
 
@@ -143,29 +115,16 @@ class Self_Awareness:
         ), dtype=np.float64)
 
 
-    def do_conversion(self):
-
-        start_time = time.time()
-
-        # pc_np = np.asarray(self.veldynes[-1])
-
-        pc_np = self.concat_scans()
-
-        bird_view_img = lidar_projection.birds_eye_point_cloud(pc_np, side_range=(-50, 50), fwd_range=(-50, 50), res=0.25, min_height=-8, max_height=20)
-        # print("bird-view image shape: ", bird_view_img.shape)
-        cv2.imwrite(os.path.join("tmp.png"), bird_view_img)
-
-        # print("[inference_node]: runing time = " + str(time.time() - start_time))
-
     def save_vels(self, vels, file_name='tmp.png'):
 
         start_time = time.time()
-        bird_view_img = lidar_projection.birds_eye_point_cloud(vels, side_range=(-50, 50), fwd_range=(-50, 50),
-                                                               res=0.25, min_height=-8, max_height=20)
-        # print("bird-view image shape: ", bird_view_img.shape)
+        # 50, 50 res = 0.25 / 100, 100 res=0.5
+        bird_view_img = lidar_projection.birds_eye_point_cloud(vels, side_range=(-100, 100), fwd_range=(-100, 100),
+                                                               res=0.5, min_height=-8, max_height=20)
+        print("bird-view image shape: ", bird_view_img.shape)
         cv2.imwrite(file_name, bird_view_img)
 
-        # print("[inference_node]: runing time = " + str(time.time() - start_time))
+        print("[inference_node]: runing time = " + str(time.time() - start_time))
 
 
 if __name__ == '__main__':
